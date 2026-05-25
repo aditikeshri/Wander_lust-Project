3e0f273a331d0383d00be5f861f564ad010ca5c2
aditikeshri <aditikeshri2302@gmail.com>
2026-05-25 21:24:16 +0530

    fixed session store error

--- app.js
+++ app.js
@@ -46,11 +46,16 @@ app.use(methodOverride("_method"));
 app.engine('ejs',ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
+// const store = MongoStore.create({
+//     mongoUrl: dbUrl,
+//     crypto: {
+//         secret: process.env.SECRET,
+//     },
+// });
+
 const store = MongoStore.create({
     mongoUrl: dbUrl,
-    crypto: {
-        secret: process.env.SECRET,
-    },
+    touchAfter: 24 * 3600,
 });
 
 
